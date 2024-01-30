d1d923f72f33f3e1e75dcb32493961f5bbee8a9b
Alan Nguyen <alan@mirk.co>
2024-01-30 12:16:18 +1100

    update: add error response to cloudfront config

--- modules/StaticWebsite/CloudfrontSetup/cloudfront.tf
+++ modules/StaticWebsite/CloudfrontSetup/cloudfront.tf
@@ -2,6 +2,9 @@ locals {
   s3_origin_id = "myS3Origin"
 }
 
+data "aws_cloudfront_cache_policy" "cache-optimized" {
+  name = "Managed-CachingOptimized"
+}
 resource "aws_cloudfront_origin_access_control" "cf_oac" {
   name                              = "Website"
   description                       = "Example Policy"
@@ -28,64 +31,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
+    compress = true
+    cache_policy_id = data.aws_cloudfront_cache_policy.cache-optimized.id
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
   }
 
   price_class = "PriceClass_All"
@@ -105,5 +56,18 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     acm_certificate_arn = var.certArn
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method = "sni-only"
+  }
+
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+    error_caching_min_ttl = 60
+  }
+    custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+    error_caching_min_ttl = 60
   }
 }

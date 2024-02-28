6941f272ecd098daffa7a12ea0b9cdcb96b4f4e7
Alan Nguyen <alan@mirk.co>
2024-02-29 10:51:53 +1100

    Fix: Got scripts running

--- modules/ECS/Cluster/autoscalinggroup.tf
+++ modules/ECS/Cluster/autoscalinggroup.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "ecs_asg" {
     name = "PlayTodayASG"
-    desired_capacity    = 2
+    desired_capacity    = 1
     min_size = 1
     max_size = 3
 
--- modules/ECS/Cluster/loadbalancer.tf
+++ modules/ECS/Cluster/loadbalancer.tf
@@ -14,7 +14,7 @@ resource "aws_alb_target_group" "default-target-group" {
   port = 80
   protocol = "HTTP"
   vpc_id = aws_vpc.pt_vpc.id
-
+  target_type = "ip"
   health_check {
     path = var.health_check_path
     port = "traffic-port"
